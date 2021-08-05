0c89cdf702b62d89263b48728774c8ddedd21e59
nishinoshungo <nishinoshungo>
2021-08-05 13:21:40 +0000

    add order index

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -58,6 +58,7 @@ class Public::OrdersController < ApplicationController
 
 
   def index
+    @orders = Order.all
   end
 
   def show
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,12 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   enum payment: {クレジットカード: 0, 銀行振込: 1}
-
+  enum order_status: {
+    入金待ち: 1,
+    入金確認: 2,
+    製作中: 3,
+    発送準備中: 4,
+    発送済み: 5
+  }
 
 end
