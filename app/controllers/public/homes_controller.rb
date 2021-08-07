d5ce3feac5cc18e8d2388a51a3bd5b6915c18694
nishinoshungo <nishinoshungo>
2021-08-07 15:39:03 +0000

    販売停止中商品の非表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @items = Item.order(created_at: :desc).limit(4)
+    @items = Item.order(created_at: :desc).where.not(is_active: false).limit(4)
     @genres = Genre.all
   end
 
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,10 +2,10 @@ class Public::ItemsController < ApplicationController
   def index
     if params[:search] == "genre"
       genre = Genre.find(params[:genre_id])
-      @items = genre.items
+      @items = genre.items.where.not(is_active: false)
       @genre_name = genre.name
     else
-      @items = Item.all
+      @items = Item.where.not(is_active: false)
       @genre_name = "商品"
     end
     @genres = Genre.all
@@ -13,7 +13,7 @@ class Public::ItemsController < ApplicationController
 
   def search
     if params[:keyword].present?
-      @items = Item.where('name LIKE ?', "%#{params[:keyword]}%")
+      @items = Item.where('name LIKE ?', "%#{params[:keyword]}%").where.not(is_active: false)
     else
       @items = Item.none
     end
@@ -27,5 +27,9 @@ class Public::ItemsController < ApplicationController
     @tax_include_price = @item.price * 1.1
     @cart_item = CartItem.new
     @genres = Genre.all
+
+    if @item.is_active == false
+      redirect_to items_path
+    end
   end
 end
