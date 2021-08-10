df06227b6807209c8959aa4a8585584ef5504852
nishinoshungo <nishinoshungo>
2021-08-10 11:38:30 +0000

    注文ステータスと製作ステータスの連動

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -4,6 +4,23 @@ class Admin::OrderItemsController < ApplicationController
   def update
     order_item = OrderItem.find(params[:id])
     order_item.update(order_item_params)
+    order = order_item.order
+    if order_item.making_status == "製作中"
+      order.order_status = 3
+      order.save
+    end
+
+    order_items = order_item.order.order_items
+    order_items.each do |order_item|
+      if order_item.making_status != "制作完了"
+        redirect_to admin_order_path(order_item.order.id)
+        return
+      elsif order_item.making_status == "制作完了"
+        order.order_status = 4
+      end
+    end
+    order.save
+
     redirect_to admin_order_path(order_item.order.id)
   end
 
